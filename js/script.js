5c496fa921c8150caf294d9193adffa2fbbe4f94
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-09 18:18:41 +0100

    print result on screen, modify element. Fix braces for single values

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 // get date of countdown in milliseconds
-let targetDate = new Date("Feb 12, 09:30:00").getTime();
-console.log(date);
+let targetDate = new Date("Feb 12 2024, 09:30:00").getTime();
+console.log(targetDate);
 
-let actualDate = Date.now()
-console.log(actualDate);
+// set an interval for 1s
+let countdown = setInterval(function () {
+    // take the actual date in milliseconds
+    let actualDate = Date.now();
+    console.log(actualDate);
+
+    // this will be calculated subtract the target time with the actual date
+    let difference = targetDate - actualDate;
+
+    // get single value days, hours, minutes and seconds and convert in "real time"
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor(difference % (1000 * 60 * 60) / (1000 * 60));
+    let seconds = Math.floor(difference % (1000 * 60) / 1000);
+    document.querySelector("#countdown").innerHTML = `${days} ${hours} ${minutes} ${seconds}`;
+
+}, 1000)
+
+
+
 
 
 
