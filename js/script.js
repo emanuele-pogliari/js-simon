d9c800907d4b6bf79f1a6b911222a3a68cdc6ce5
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-10 14:24:37 +0100

    wip: fix circles and adapt font

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 // get date of countdown in milliseconds
 let targetDate = new Date("Feb 12 2024, 09:30:00").getTime();
-console.log(targetDate);
 
 // set an interval for 1s
 let countdown = setInterval(function () {
     // take the actual date in milliseconds
     let actualDate = Date.now();
-    console.log(actualDate);
 
     // this will be calculated subtract the target time with the actual date
     let difference = targetDate - actualDate;
