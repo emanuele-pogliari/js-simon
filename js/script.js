c8dcba9a572b9d1bb4dee43393519e1e92034db7
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-10 15:36:23 +0100

    changed stroke color and fireworks added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 // get date of countdown in milliseconds
 let targetDate = new Date("Feb 12 2024, 09:30:00").getTime();
 
+let circleDaysStroke = document.querySelector("#dd");
+let circleHoursStroke = document.querySelector("#hh");
+let circleMinutesStroke = document.querySelector("#min");
+let circleSecondsStroke = document.querySelector("#sec");
+
+let circleDays = document.querySelector("#days");
+let circleHours = document.querySelector("#hours");
+let circleMinutes = document.querySelector("#minutes");
+let circleSeconds = document.querySelector("#seconds");
+
 // set an interval for 1s
 let countdown = setInterval(function () {
     // take the actual date in milliseconds
@@ -29,6 +39,10 @@ let countdown = setInterval(function () {
 
         document.querySelector("#countdown").innerHTML = `- ${0} ${0} ${0} ${0}`;
     }
+    circleDaysStroke.style.strokeDashoffset = 440 - (440 * days) / 365;
+    circleHoursStroke.style.strokeDashoffset = 440 - (440 * hours) / 24;
+    circleMinutesStroke.style.strokeDashoffset = 440 - (440 * minutes) / 60;
+    circleSecondsStroke.style.strokeDashoffset = 440 - (440 * seconds) / 60;
 }, 1000)
 
 
@@ -45,8 +59,9 @@ function checkPlural(days, hours, minutes, seconds, words) {
     if (days == 1) {
         words[3] = "giorno";
     }
-
 }
+
+
 
 
 
